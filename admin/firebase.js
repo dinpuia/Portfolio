bdbd528946af4fd6f600145a261233ae46b27c01
dinpuia <42483178+dinpuia@users.noreply.github.com>
2025-04-18 00:33:00 +0530

    Create firebase.js

---
+++ admin/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDeXPI59Y1t3EqcS11_05Qyc-i16o5iriQ",
+  authDomain: "endless-grafix-admin-5fff7.firebaseapp.com",
+  projectId: "endless-grafix-admin-5fff7",
+  storageBucket: "endless-grafix-admin-5fff7.firebasestorage.app",
+  messagingSenderId: "685694239178",
+  appId: "1:685694239178:web:e3bbe47e8c32a0df4f8f33",
+  measurementId: "G-T41P34TGKQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
