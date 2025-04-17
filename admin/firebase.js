fd7bdbe9bc1a5ef8842792f65c0ae853d94b1c70
dinpuia <42483178+dinpuia@users.noreply.github.com>
2025-04-18 01:00:57 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,13 +1,11 @@
+
 const firebaseConfig = {
   apiKey: "AIzaSyDeXPI59Y1t3EqcS11_05Qyc-i16o5iriQ",
   authDomain: "endless-grafix-admin-5fff7.firebaseapp.com",
   projectId: "endless-grafix-admin-5fff7",
-  storageBucket: "endless-grafix-admin-5fff7.firebasestorage.app",
+  storageBucket: "endless-grafix-admin-5fff7.appspot.com",
   messagingSenderId: "685694239178",
-  appId: "1:685694239178:web:e3bbe47e8c32a0df4f8f33",
-  measurementId: "G-T41P34TGKQ"
+  appId: "1:685694239178:web:e3bbe47e8c32a0df4f8f33"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
